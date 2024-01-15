460a7d0a4fa0bbdc89d1261aeb57fe9feb7c0ff2
Dave <d.ruocco1987@googlemail.com>
2024-01-15 12:59:41 +0000

    Sets up main program flow

--- Program.cs
+++ Program.cs
@@ -21,7 +21,28 @@
 
             public void Run()
             {
+                var allRecipes = _recipeRepository.Read(filePath);
+                _recipeUserInteraction.PrintExistingRecipes(allRecipes);
 
+                _recipeUserInteraction.PromptToCreateRecipe();
+
+                var ingredients = _recipeUserInteraction.ReadIngredientsFromUser();
+
+                if(ingredients.Count > 0)
+                {
+                    var recipes = new CookiesRecipeApp(ingredients);
+                    allRecipes.Add(recipes);
+                    _recipeRepository.Write(filePath, allRecipes);
+
+                    _recipeUserInteraction.ShowMessage("Recipe added.");
+                    _recipeUserInteraction.ShowMessage(recipe.ToString());
+                }
+                else
+                {
+                    _recipeUserInteraction.ShowMessage(
+                        "No ingredients selected." + "Recipe will not be saved");
+                }
+                _recipeUserInteraction.Exit();
             }
         }
     }
