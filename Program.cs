c5cccf15db241f65a55620064778043fccfe21b5
Dave <d.ruocco1987@googlemail.com>
2024-01-15 12:45:06 +0000

    Initial commit, sets up project

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CookiesCookbook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookiesRecipeApp = new CookiesRecipeApp();
+            cookiesRecipeApp.Run();
+        }
+
+        class CookiesRecipeApp
+        {
+            private readonly RecipeRepository _recipeRepository;
+            private readonly RecipeUserInteraction _recipeUserInteraction;
+
+            public CookiesRecipeApp(RecipeRepository recipeRepository, RecipeUserInteraction recipeUserInteraction)
+            {
+                _recipeRepository = recipeRepository;
+                _recipeUserInteraction = recipeUserInteraction;
+            }
+
+            public void Run()
+            {
+
+            }
+        }
+    }
+}
